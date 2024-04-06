9fd32d95b461cfd011369556381c1092f99cfc8d
Vladimir Chim <vladimir.chim@indrivo.com>
2024-04-06 11:53:51 +0300

    Add project files.

---
+++ CompanyManagement.API/Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/companies")]
+    public class CompanyController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetCompanies()
+        {
+            return Empty;
+        }
+
+        [HttpPost]
+        public ActionResult AddCompany()
+        {   
+            return Empty;
+        }
+    }
+}
---
+++ CompanyManagement.API/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("test")]
+        public ActionResult GetEmployees()
+        {   
+            return Empty;
+        }
+
+        [HttpPost("addEmployee")]
+        public ActionResult AddEmployee([FromBody] int x, [FromBody] int y)
+        {           
+            return Empty;
+        }
+    }
+}
